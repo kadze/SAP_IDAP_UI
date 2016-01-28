ca02a9fc824b68486d7bebd1d55dc8b4bd9baff9
SAP <shevtsov.ap@gmail.com>
2016-01-28 06:03:00 -0800

    SAPSquareView

---
+++ IOSProject/Source/UI/SAPSquareViewController/SAPSquareView.m
@@ -0,0 +1,21 @@
+//
+//  SAPSquareView.m
+//  IOSProject
+//
+//  Created by S A P on 1/28/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import "SAPSquareView.h"
+
+@implementation SAPSquareView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
