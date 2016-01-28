275d59073f05de0fb5f5451024a31068c56a4293
SAP <shevtsov.ap@gmail.com>
2016-01-28 01:45:54 -0800

    Poject created

---
+++ IOSProject/IOSProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSProject
+//
+//  Created by S A P on 1/28/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
