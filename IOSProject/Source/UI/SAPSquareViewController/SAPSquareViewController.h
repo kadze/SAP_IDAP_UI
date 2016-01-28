caa59d1cc7b199f88665a1a1393ddb998600ff3b
SAP <shevtsov.ap@gmail.com>
2016-01-28 05:17:10 -0800

    add SAPSquareViewController.h; remove main.storyboard

---
+++ IOSProject/Source/UI/SAPSquareViewController/SAPSquareViewController.h
@@ -0,0 +1,13 @@
+//
+//  SAPSquareViewController.h
+//  IOSProject
+//
+//  Created by S A P on 1/28/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SAPSquareViewController : UIViewController
+
+@end
