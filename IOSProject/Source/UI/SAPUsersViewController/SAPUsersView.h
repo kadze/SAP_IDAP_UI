2b71c6d9abbcf46ea277cae2282b12b973916082
SAP <shevtsov.ap@gmail.com>
2016-02-12 04:04:33 -0800

    usersView -> tableView

--- IOSProject/Source/UI/SAPUsersViewController/SAPUsersView.h
+++ IOSProject/Source/UI/SAPUsersViewController/SAPUsersView.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface SAPUsersView : UIView
-@property (nonatomic, strong) IBOutlet UITableView *usersView;
+@property (nonatomic, strong) IBOutlet UITableView  *tableView;
 @property (nonatomic, strong) IBOutlet UIButton     *addUserButton;
 
 - (IBAction)onLongPress:(UILongPressGestureRecognizer *)sender;
