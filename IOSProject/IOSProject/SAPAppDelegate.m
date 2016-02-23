6648ceca1c726560aafc8374d6d62829d200065e
SAP <shevtsov.ap@gmail.com>
2016-02-23 05:34:03 -0800

    app saveData method

--- IOSProject/IOSProject/SAPAppDelegate.m
+++ IOSProject/IOSProject/SAPAppDelegate.m
@@ -14,6 +14,7 @@
 #import "UIWindow+SAPExtensions.h"
 
 @interface SAPAppDelegate ()
+- (void) saveData;
 
 @end
 
@@ -42,7 +43,7 @@
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    [((SAPUsersViewController *)self.window.rootViewController).users save];
+    [self saveData];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
@@ -54,7 +55,14 @@
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    
+
+}
+
+#pragma mark -
+#pragma mark Private
+
+- (void)saveData {
+    [((SAPUsersViewController *)self.window.rootViewController).users save];
 }
 
 @end
