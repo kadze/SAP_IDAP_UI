b74701e2d53ac21f31ea42393a4b5077af8bd7c9
SAP <shevtsov.ap@gmail.com>
2016-03-07 08:44:39 -0800

    NSURL+SAPExtensions

---
+++ IOSProject/Source/Library/Categories/NSURL/NSURL+SAPExtensions.m
@@ -0,0 +1,25 @@
+//
+//  NSURL+SAPExtensions.m
+//  IOSProject
+//
+//  Created by S A P on 3/7/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import "NSURL+SAPExtensions.h"
+
+static NSString * const kSAPQuestionMark = @"?";
+static NSString * const kSAPQuestionMarkSubstitute = @"SAPQuestionMark";
+static NSString * const kSAPSlash = @"/";
+static NSString * const kSAPSlashSubstitute = @"SAPSlash";
+
+@implementation NSURL (SAPExtensions)
+
+- (NSString *)convertIntoFilename {
+    NSString *result = [self absoluteString];
+    result = [result stringByReplacingOccurrencesOfString:kSAPQuestionMark withString:kSAPQuestionMarkSubstitute];
+   
+    return [result stringByReplacingOccurrencesOfString:kSAPSlash withString:kSAPSlashSubstitute];
+}
+
+@end
