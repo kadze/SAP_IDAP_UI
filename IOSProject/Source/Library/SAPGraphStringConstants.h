37e3ee3fe1ed2c3741bf1da0556616d72450146d
SAP <shevtsov.ap@gmail.com>
2016-03-23 03:34:30 -0700

    SAPGraphStringConstants add constants

--- IOSProject/Source/Library/SAPGraphStringConstants.h
+++ IOSProject/Source/Library/SAPGraphStringConstants.h
@@ -16,5 +16,13 @@ static NSString * const kSAPFriendsKey = @"friends";
 static NSString * const kSAPIDKey = @"id";
 static NSString * const kSAPFirstNameKey = @"first_name";
 static NSString * const kSAPLastNameKey = @"last_name";
+static NSString * const kSAPGengerKey = @"gender";
+
 static NSString * const kSAPPictureKey = @"picture";
+static NSString * const kSAPLagrePictureKey = @"picture.type(lagre)";
+static NSString * const kSAPSmallPictureKey = @"picture.type(small)";
+static NSString * const kSAPNormalPictureKey = @"picture.type(normal)";
+static NSString * const kSAPAlbumPictureKey = @"picture.type(album)";
+static NSString * const kSAPSquarePictureKey = @"picture.type(square)";
+
 static NSString * const kSAPUrlKey = @"url";
