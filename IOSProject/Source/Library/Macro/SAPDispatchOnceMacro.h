b8bcd705aa0ab1ebaf10c8d9835c00584fd29499
SAP <shevtsov.ap@gmail.com>
2016-04-07 06:09:41 -0700

    SAPNilToNSNullMacro rename.  No guards in macro in this project

--- IOSProject/Source/Library/Macro/SAPDispatchOnceMacro.h
+++ IOSProject/Source/Library/Macro/SAPDispatchOnceMacro.h
@@ -6,13 +6,8 @@
 //  Copyright © 2016 SAP. All rights reserved.
 //
 
-#ifndef SAPDispatchOnceMacro_h
-#define SAPDispatchOnceMacro_h
-
 #define SAPDispatchOnce(block) \
     do { \
         static dispatch_once_t onceToken; \
         dispatch_once(&onceToken, block); \
     } while(0);
-
-#endif /* SAPDispatchOnceMacro_h */
--- IOSProject/Source/Library/Macro/SAPNilToNSNullMacro.h
+++ IOSProject/Source/Library/Macro/SAPNilToNSNullMacro.h
@@ -6,13 +6,8 @@
 //  Copyright © 2016 SAP. All rights reserved.
 //
 
-#ifndef SAPCompareMacro_h
-#define SAPCompareMacro_h
-
 #define SAPNilIfNSNull(value) \
     ([NSNull new] == value) ? nil : value
 
 #define SAPNSNullIfNil(value) \
     nil == value ? [NSNull new] : value
-
-#endif /* SAPCompareMacro_h */
