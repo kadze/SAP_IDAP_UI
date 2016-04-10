679ccf6d693a7454d43fed5cf40dfd2717720b59
SAP <shevtsov.ap@gmail.com>
2016-04-10 10:42:55 -0700

    SAPContextSetterMacro.h

---
+++ IOSProject/Source/Library/Macro/SAPContextSetterMacro.h
@@ -0,0 +1,13 @@
+//
+//  SAPContextSetterMacro.h
+//  IOSProject
+//
+//  Created by SAP on 4/10/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#ifndef SAPContextSetterMacro_h
+#define SAPContextSetterMacro_h
+
+
+#endif /* SAPContextSetterMacro_h */
