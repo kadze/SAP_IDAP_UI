b6f14e1bfacd0083dc857acfa0d1f797f73af045
SAP <shevtsov.ap@gmail.com>
2016-04-11 22:27:51 +0300

    SAPGraphStringConstants constants for square pictures

--- IOSProject/Source/Library/SAPGraphStringConstants.h
+++ IOSProject/Source/Library/SAPGraphStringConstants.h
@@ -20,14 +20,24 @@ SAPNSStringConst(kSAPFirstNameKey, @"first_name");
 SAPNSStringConst(kSAPLastNameKey, @"last_name");
 SAPNSStringConst(kSAPGenderKey, @"gender");
 
+/*
+ 
+ square (50x50)
+ small (50 pixels wide, variable height)
+ normal (100 pixels wide, variable height), and
+ large (about 200 pixels wide, variable height)
+*/
+
 SAPNSStringConst(kSAPPictureKey, @"picture");
 SAPNSStringConst(kSAPLargePictureKey, @"picture.type(large)");
-SAPNSStringConst(kSAPSmallPictureKey, @"picture.type(small)");
+SAPNSStringConst(kSAPLargeSquarePictureKey, @"picture.width(200).height(200)");
+SAPNSStringConst(kSAPSmallPictureKey, @"picture.type(square)");
 SAPNSStringConst(kSAPNormalPictureKey, @"picture.type(normal)");
 SAPNSStringConst(kSAPAlbumPictureKey, @"picture.type(album)");
 SAPNSStringConst(kSAPSquarePictureKey, @"picture.type(square)");
 
 SAPNSStringConst(kSAPLargePictureWithAliasKey, @"picture.type(large).as(picture_large)");
+SAPNSStringConst(kSAPLargeSquarePictureWithAliasKey, @"picture.width(200).height(200).as(picture_large)");
 SAPNSStringConst(kSAPSmallPictureWithAliasKey, @"picture.type(small).as(picture_small)");
 SAPNSStringConst(kSAPNormalPictureWithAliasKey, @"picture.type(normal).as(picture_normal)");
 SAPNSStringConst(kSAPAlbumPictureWithAliasKey, @"picture.type(album).as(picture_album)");
