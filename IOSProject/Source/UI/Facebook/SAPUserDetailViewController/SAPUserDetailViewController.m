55f79ad1dffca9494646f2bfcb4c081dc6067ddb
SAP <shevtsov.ap@gmail.com>
2016-04-14 16:30:29 +0300

    SAPUserDetailViewController modelConetxt

--- IOSProject/Source/UI/Facebook/SAPUserDetailViewController/SAPUserDetailViewController.m
+++ IOSProject/Source/UI/Facebook/SAPUserDetailViewController/SAPUserDetailViewController.m
@@ -9,16 +9,22 @@
 #import "SAPUserDetailViewController.h"
 
 #import "SAPUserDetailView.h"
-#import "SAPUser.h"
 #import "SAPUserDetailContext.h"
-
-#import "SAPDispatch.h"
 
 #import "SAPViewControllerMacro.h"
 
 SAPViewControllerBaseViewProperty(SAPUserDetailViewController, SAPUserDetailView, mainView);
 
 @implementation SAPUserDetailViewController
+
+@dynamic modelContext;
+
+#pragma mark -
+#pragma mark Accessors
+
+- (SAPContext *)modelContext {
+    return [SAPUserDetailContext contextWithModel:self.model];
+}
 
 #pragma mark -
 #pragma mark Public
@@ -28,8 +34,7 @@ SAPViewControllerBaseViewProperty(SAPUserDetailViewController, SAPUserDetailView
 }
 
 - (void)finishModelSetting {
-    SAPUserDetailContext *context = [SAPUserDetailContext contextWithModel:self.model];
-    self.context = context;
+    self.context = [self modelContext];
 }
 
 @end
