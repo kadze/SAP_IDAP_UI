d4a2aa8ffc328f14a981521a81a1c495a55d958d
SAP <shevtsov.ap@gmail.com>
2016-04-15 22:11:23 +0300

    SAPSetters.h

---
+++ IOSProject/Source/Library/Functions/SAPSetters/SAPSetters.h
@@ -0,0 +1,13 @@
+//
+//  SAPSetters.h
+//  IOSProject
+//
+//  Created by Andrey on 4/15/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAPSetters : NSObject
+
+@end
---
+++ IOSProject/Source/Library/Functions/SAPSetters/SAPSetters.m
@@ -0,0 +1,13 @@
+//
+//  SAPSetters.m
+//  IOSProject
+//
+//  Created by Andrey on 4/15/16.
+//  Copyright © 2016 SAP. All rights reserved.
+//
+
+#import "SAPSetters.h"
+
+@implementation SAPSetters
+
+@end
