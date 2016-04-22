27bf2968aab3543139fede87cdadeb749e89eb0e
SAP <shevtsov.ap@gmail.com>
2016-04-22 23:37:28 +0300

    NSFetchedResultsController in SAPUsers

--- IOSProject/Source/Models/SAPUsers/SAPUsers.h
+++ IOSProject/Source/Models/SAPUsers/SAPUsers.h
@@ -8,8 +8,14 @@
 
 #import "SAPArrayModel.h"
 
+#import <CoreData/NSFetchedResultsController.h>
+
 #import "SAPCacheableModel.h"
 
-@interface SAPUsers : SAPArrayModel <SAPCacheableModel>
+@interface SAPUsers : SAPArrayModel <SAPCacheableModel, NSFetchedResultsControllerDelegate>
+@property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+
+- (instancetype)initWithFetchedResultsControllerForContext:(NSManagedObjectContext *)context
+                                                    entity:(NSEntityDescription *)entity;
 
 @end
--- IOSProject/Source/Models/SAPUsers/SAPUsers.m
+++ IOSProject/Source/Models/SAPUsers/SAPUsers.m
@@ -10,6 +10,41 @@
 
 #import "SAPUser.h"
 
+static NSString * const kSAPDescriptorKey = @"firstName";
+
 @implementation SAPUsers
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (instancetype)initWithFetchedResultsControllerForContext:(NSManagedObjectContext *)context
+                                            entity:(NSEntityDescription *)entity
+{
+    self = [super init];
+    NSFetchRequest *fetchedRequest = [NSFetchRequest fetchRequestWithEntityName:entity.managedObjectClassName];
+    fetchedRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:kSAPDescriptorKey ascending:YES]];
+    
+    self.fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchedRequest
+                                                                        managedObjectContext:context
+                                                                          sectionNameKeyPath:nil cacheName:nil];;
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark NSFetchedResultsControllerDelegate
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(nullable NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(nullable NSIndexPath *)newIndexPath
+{
+    if (NSFetchedResultsChangeInsert == type) {
+        [self performBlockWithoutNotification:^{
+            [self addObject:anObject];
+        }];
+    }
+}
 
 @end
